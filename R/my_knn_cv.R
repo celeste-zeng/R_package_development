2b44afa28f0ac1bb3b69a458e47c30ff196bab72
Celeste Zeng <czeng@uw.edu>
2020-03-06 15:21:23 -0800

    commit changes

--- R/my_knn_cv.R
+++ R/my_knn_cv.R
@@ -13,16 +13,18 @@
 #'  @examples
 #'  my_knn_cv(train = my_iris[, -5], cl = my_iris$Species, k_nn = 5, k_cv = 5)
 #'
-#' @import class
+#'  @import class
 #'
 #'  @export
+
+
 my_knn_cv <- function(train, cl, k_nn, k_cv) {
   folds <- sample(rep(1 : k_cv, length = nrow(train)))
   err <- c()
   for (i in 1 : k_cv) {
     data_train <- train[folds != i, ] #X_i
     data_test <-  train[folds == i, ] #X_i*
-    cl_train <- cl[folds != i] #Y_i
+    cl_train <- cl[folds != i] #Y_idebug
     cl_test <-  cl[folds == i] #Y_i*
     knn_output <- knn(train = data_train, cl = cl_train, test  = data_test, k = k_nn) #Y_i*^
     err[i] <- sum(knn_output != cl_test) / length(cl_test)
