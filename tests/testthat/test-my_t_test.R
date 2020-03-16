b3e54d7941d940c0692b7633d747aefe09cd1c6b
Celeste Zeng <czeng@uw.edu>
2020-03-16 16:59:39 -0700

    commit changes

--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -2,3 +2,11 @@
 test_that("my_t_test outputs a list", {
   expect_is(my_t_test(c(1, 2, 3, 4, 5), mu = 3, alternative = "greater"), "list")
 })
+
+test_that("my_t_test outputs a list", {
+  expect_is(my_t_test(c(1, 2, 3, 4, 5), mu = 3, alternative = "less"), "list")
+})
+
+test_that("my_t_test outputs a list", {
+  expect_is(my_t_test(c(1, 2, 3, 4, 5), mu = 3, alternative = "two.sided"), "list")
+})
